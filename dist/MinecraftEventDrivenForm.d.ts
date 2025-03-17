407fc03778db6446733d467702a483ef397ac59d
josejulio <3845764+josejulio@users.noreply.github.com>
2025-03-17 04:19:49 +0000

    Updates `./dist/*` files with latest changes

---
+++ dist/MinecraftEventDrivenForm.d.ts
@@ -0,0 +1,146 @@
+interface Translate {
+    translate: string;
+    args?: Array<TextContent>;
+}
+type TextContent = string | Translate | Array<TextContent>;
+type NormalizedTextContent = {
+    type: "translate";
+    translate: string;
+    args?: Array<string> | Array<NormalizedTextContent>;
+} | {
+    type: "text";
+    text: string;
+} | {
+    type: "array";
+    array: Array<NormalizedTextContent>;
+};
+interface FormAction {
+    form?: string;
+    event?: string;
+    eventArgs?: Array<unknown>;
+    copyArgs?: boolean;
+}
+interface DualButtonForm {
+    type: "dual-button";
+    title: TextContent;
+    body?: TextContent;
+    topButton: DualButtonElementButton;
+    bottomButton: DualButtonElementButton;
+}
+type DualButtonElement = DualButtonElementButton;
+interface DualButtonElementButton {
+    type: "button";
+    text: TextContent;
+    action?: FormAction;
+}
+interface InputForm {
+    type: "input";
+    title: TextContent;
+    submit?: TextContent;
+    elements: Array<InputElement>;
+    action?: FormAction;
+}
+type InputValue = string | number | boolean;
+type InputElement = InputElementSlider | InputElementDropdown | InputElementText | InputElementToggle;
+type InputElementSlider = {
+    type: "slider";
+    name?: string;
+    text: TextContent;
+    min: number;
+    max: number;
+    step: number;
+    defaultValue?: number;
+};
+type InputElementDropdown = {
+    type: "dropdown";
+    name?: string;
+    text: TextContent;
+    defaultValue?: InputValue;
+    options: Array<{
+        text: TextContent;
+        value: InputValue;
+    }>;
+};
+type InputElementText = {
+    type: "text";
+    name?: string;
+    text: TextContent;
+    placeholder: TextContent;
+    defaultValue?: string;
+};
+type InputElementToggle = {
+    type: "toggle";
+    name?: string;
+    text: TextContent;
+    defaultValue?: boolean;
+};
+interface MultiButtonForm {
+    type: "multi-button";
+    title: TextContent;
+    body?: TextContent;
+    elements: Array<MultiButtonElement>;
+}
+type MultiButtonElement = MultiButtonElementButton;
+interface MultiButtonElementButton {
+    type: "button";
+    text: TextContent;
+    icon?: string;
+    action?: FormAction;
+}
+type Form = MultiButtonForm | InputForm | DualButtonForm;
+interface ToString {
+    toString(): string;
+}
+type StringResolvableMap = {
+    [key: string]: StringResolvable;
+};
+type StringResolvable = ToString | StringResolvableMap;
+declare class FormArguments {
+    private _args;
+    set(name: string, arg: StringResolvable): void;
+    setAll(args: Record<string, StringResolvable>): void;
+    getAll(): Record<string, StringResolvable>;
+    get<Arg extends StringResolvable>(name: string): Arg;
+    resolvePath(path: string): string;
+    resolveTemplate(template: string): string;
+    normalize(content: TextContent): NormalizedTextContent;
+}
+declare class FormError extends Error {
+    constructor(msg: string);
+}
+declare class FormArgumentError extends FormError {
+    readonly path: string;
+    readonly step: string;
+    readonly current: unknown;
+    constructor(path: string, step: string, current: unknown);
+}
+interface FormHub {
+    entrypoint: string;
+    forms: Record<string, Form>;
+}
+declare class FormEvent {
+    protected _form: Form | undefined;
+    protected _name: string | undefined;
+    protected _continueProcessing: boolean;
+    protected readonly _hub: FormHub;
+    protected _args: FormArguments;
+    protected _eventArgs: Array<unknown>;
+    constructor(hub: FormHub, action?: FormAction, previousArgs?: FormArguments);
+    loadForm(name: string): Form;
+    loadForm(name: string, type: "multi-button"): MultiButtonForm;
+    loadForm(name: string, type: "input"): InputForm;
+    loadForm(name: string, type: "dual-button"): InputForm;
+    set form(form: Form | undefined);
+    get form(): Form | undefined;
+    get name(): string | undefined;
+    get args(): FormArguments;
+    get eventArgs(): unknown[];
+    get continueProcessing(): boolean;
+    cancelProcessing(): void;
+}
+type EventReceiverFunction = (event: FormEvent) => Promise<void>;
+type EventReceiverMap = Record<string, EventReceiverFunction>;
+type EventReceiver = EventReceiverFunction | EventReceiverMap | undefined;
+declare const triggerEvent: (event: FormEvent, receiver: EventReceiver) => Promise<Form | undefined>;
+declare const _: (value: string) => Translate;
+export { InputForm, MultiButtonForm, MultiButtonElement, MultiButtonElementButton, DualButtonForm, DualButtonElement, DualButtonElementButton, Form, StringResolvable, FormArguments, FormError, FormArgumentError, FormEvent, EventReceiver, triggerEvent, FormHub, Translate, TextContent, NormalizedTextContent, FormAction, _ };
---
+++ dist/MinecraftEventDrivenForm.js
@@ -0,0 +1,159 @@
+class FormError extends Error {
+    constructor(msg) {
+        super(msg);
+    }
+}
+class FormArgumentError extends FormError {
+    path;
+    step;
+    current;
+    constructor(path, step, current) {
+        super(`Invalid path: ${path} at step: ${step} in object: ${JSON.stringify(current)}`);
+        this.path = path;
+        this.step = step;
+        this.current = current;
+    }
+}
+
+class FormArguments {
+    _args = {};
+    set(name, arg) {
+        this._args[name] = arg;
+    }
+    setAll(args) {
+        this._args = {
+            ...this._args,
+            ...args,
+        };
+    }
+    getAll() {
+        return this._args;
+    }
+    get(name) {
+        return this._args[name];
+    }
+    resolvePath(path) {
+        let current = this._args;
+        const splitPath = path.split('.');
+        for (const step of splitPath) {
+            if (typeof current === 'object' && step in current) {
+                current = current[step];
+            }
+            else {
+                throw new FormArgumentError(path, step, current);
+            }
+        }
+        return current.toString();
+    }
+    resolveTemplate(template) {
+        return template.replace(/\{\s*([^}\s]+)\s*\}/g, (_, p1) => {
+            return this.resolvePath(p1);
+        });
+    }
+    normalize(content) {
+        if (typeof content === 'string') {
+            return {
+                type: 'text',
+                text: this.resolveTemplate(content),
+            };
+        }
+        else if (Array.isArray(content)) {
+            return {
+                type: 'array',
+                array: content.map((c) => this.normalize(c)),
+            };
+        }
+        else {
+            return {
+                type: 'translate',
+                translate: this.resolveTemplate(content.translate),
+                args: content.args ? content.args.map((a) => this.normalize(a)) : undefined,
+            };
+        }
+    }
+}
+
+class FormEvent {
+    _form = undefined;
+    _name = undefined;
+    _continueProcessing = true;
+    _hub;
+    _args = new FormArguments();
+    _eventArgs = [];
+    constructor(hub, action, previousArgs) {
+        this._hub = hub;
+        if (action) {
+            if (action.event) {
+                this._name = action.event;
+            }
+            if (action.form) {
+                this._form = this._hub.forms[action.form];
+            }
+            if (action.copyArgs && previousArgs) {
+                this._args.setAll(previousArgs.getAll());
+            }
+            if (action.eventArgs) {
+                this._eventArgs = action.eventArgs;
+            }
+        }
+    }
+    loadForm(name, type) {
+        if (name in this._hub.forms) {
+            const form = this._hub.forms[name];
+            if (type && form.type !== type) {
+                throw new FormError(`Invalid type ${type} for form named ${name}. The actual type is ${form.type}`);
+            }
+            return JSON.parse(JSON.stringify(form));
+        }
+        throw new FormError(`Unknown form named ${name}`);
+    }
+    set form(form) {
+        this._form = form;
+    }
+    get form() {
+        return this._form;
+    }
+    get name() {
+        return this._name;
+    }
+    get args() {
+        return this._args;
+    }
+    get eventArgs() {
+        return this._eventArgs;
+    }
+    get continueProcessing() {
+        return this._continueProcessing;
+    }
+    cancelProcessing() {
+        this._continueProcessing = false;
+    }
+}
+const triggerEvent = async (event, receiver) => {
+    if (event.name) {
+        if (receiver === undefined) {
+            return;
+        }
+        else if (typeof receiver === 'function') {
+            await receiver(event);
+        }
+        else {
+            for (const [key, eventReceiver] of Object.entries(receiver)) {
+                if (!event.continueProcessing) {
+                    break;
+                }
+                if (key === event.name) {
+                    await eventReceiver(event);
+                }
+            }
+        }
+    }
+    return event.form;
+};
+
+const _ = (value) => ({
+    translate: value,
+});
+
+export { FormArgumentError, FormArguments, FormError, FormEvent, _, triggerEvent };
+//# sourceMappingURL=MinecraftEventDrivenForm.js.map
