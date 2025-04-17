a34031f26069c52ddaef702ce901b0a563ef91e0
Josejulio Martínez <finwemartinez@gmail.com>
2025-04-17 13:08:36 -0600

    chore: fix lint

--- src/Translate.test.ts
+++ src/Translate.test.ts
@@ -1,33 +1,31 @@
-import { _ } from "./Translate";
+import { _ } from './Translate';
 
 describe('Translate', () => {
-   it('_ creates a Translate', () => {
+  it('_ creates a Translate', () => {
     expect(_('hello')).toEqual({
-        translate: 'hello'
+      translate: 'hello',
     });
-   });
+  });
 
-   it('_ accepts arguments', () => {
-    expect(_('hello', "\n", "\n\n")).toEqual({
-        translate: 'hello',
-        args: ["\n", "\n\n"]
-    })
-   });
+  it('_ accepts arguments', () => {
+    expect(_('hello', '\n', '\n\n')).toEqual({
+      translate: 'hello',
+      args: ['\n', '\n\n'],
+    });
+  });
 
-   it('_ can be nested', () => {
+  it('_ can be nested', () => {
     expect(_('hello', _('arg1'), _('arg2', 'with-arg'))).toEqual({
-        translate: 'hello',
-        args: [
-            {
-                translate: 'arg1',
-            },
-            {
-                translate: 'arg2',
-                args: [
-                    'with-arg'
-                ]
-            }
-        ]
+      translate: 'hello',
+      args: [
+        {
+          translate: 'arg1',
+        },
+        {
+          translate: 'arg2',
+          args: ['with-arg'],
+        },
+      ],
     });
-   });
+  });
 });
